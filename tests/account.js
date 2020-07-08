b59b6bebb5f41f699fdc9ff335a30bcab66716df
quytruongsts <quy.truong@saigontechnology.com>
2020-07-08 14:01:38 +0700

    update example claims

--- tests/account.js
+++ tests/account.js
@@ -17,18 +17,7 @@ class Account {
    *   loading some claims from external resources etc. based on this detail
    *   or not return them in id tokens but only userinfo and so on.
    */
-  async claims(use, scope) { // eslint-disable-line no-unused-vars
-    if (this.profile) {
-      return {
-        sub: this.accountId, // it is essential to always return a sub claim
-        email: this.profile.email,
-        email_verified: this.profile.email_verified,
-        family_name: this.profile.family_name,
-        given_name: this.profile.given_name,
-        locale: this.profile.locale,
-        name: this.profile.name,
-      };
-    }
+  async claims(use, scope, claims, rejected) { // eslint-disable-line no-unused-vars
 
     return {
       sub: this.accountId, // it is essential to always return a sub claim
@@ -49,11 +38,11 @@ class Account {
       given_name: 'John',
       locale: 'en-US',
       middle_name: 'Middle',
-      name: 'John Doe',
+      'fsi/name': 'John Doe',
       nickname: 'Johny',
       phone_number: '+49 000 000000',
       phone_number_verified: false,
-      picture: 'http://lorempixel.com/400/200/',
+      'fsi/picture': 'http://lorempixel.com/400/200/',
       preferred_username: 'johnny',
       profile: 'https://johnswebsite.com',
       updated_at: 1454704946,
@@ -82,7 +71,19 @@ class Account {
     // token is a reference to the token used for which a given account is being loaded,
     //   it is undefined in scenarios where account claims are returned from authorization endpoint
     // ctx is the koa request context
-    if (!store.get(id)) new Account(id); // eslint-disable-line no-new
+    if (!store.get(id)) new Account(id, {
+      birthdate: '1987-10-16',
+      email: 'johndoe@example.com',
+      email_verified: true,
+      family_name: 'Doe',
+      gender: 'male',
+      given_name: 'John',
+      locale: 'en-US',
+      middle_name: 'Middle',
+      name: 'John Doe',
+      nickname: 'Johny',
+      picture: 'http://lorempixel.com/400/200/'
+    }); // eslint-disable-line no-new
     return store.get(id);
   }
 }
